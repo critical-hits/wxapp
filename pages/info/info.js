db2660d605380b691cfa9bfdf1e399ebc1da67ea
zhangbo <245304321@qq.com>
2017-08-25 09:47:20 +0800

    upgrade

--- pages/info/info.js
+++ pages/info/info.js
@@ -1,103 +1,72 @@
+var util = require('../../utils/util.js');
+var date_select;
+var time_select;
+var list_item=[1];
+var count=1;
+var start_date=util.formatDate(new Date());
+var start_time=util.formatTime(new Date());
 Page({
   data: {
     showTopTips: false,
-
-    radioItems: [
-      { name: 'cell standard', value: '0' },
-      { name: 'cell standard', value: '1', checked: true }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
-
-    date: "2016-09-01",
-    time: "12:01",
-
-    countryCodes: ["+86", "+80", "+84", "+87"],
-    countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
-
-    accounts: ["微信号", "QQ", "Email"],
-    accountIndex: 0,
-
+    date:start_date,
+    time:start_time,
+    date_select:start_date,
+    time_select:start_time,
     isAgree: false
   },
+  onShow: function () {
+    this.setData({ list_item: list_item ,
+     date_select:date_select,
+     time_select:time_select});
+    
+  },
+
+  addItem: function () {
+    var newarray =[1];
+    this.setData({
+      list_item: newarray.concat(this.data.list_item)
+    });
+  },
+
   showTopTips: function () {
-    var that = this;
-    this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
-  },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value);
-
-    var radioItems = this.data.radioItems;
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value;
-    }
-
-    this.setData({
-      radioItems: radioItems
-    });
-  },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
-        }
+    wx.request({
+      url: 'http://127.0.0.1:5000/', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
       }
-    }
+    })
+    // this.setData({
+    //   showTopTips: true
+    // });
+    // setTimeout(function () {
+    //   this.setData({
+    //     showTopTips: false
 
-    this.setData({
-      checkboxItems: checkboxItems
-    });
+    //   });
+    // }, 3000);
   },
+
   bindDateChange: function (e) {
+    console.log(e.detail.value)
     this.setData({
-      date: e.detail.value
+      date_select: e.detail.value
     })
   },
+
   bindTimeChange: function (e) {
+    console.log(e.detail.value)
     this.setData({
-      time: e.detail.value
+      time_select: e.detail.value
     })
   },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
 
-    this.setData({
-      countryCodeIndex: e.detail.value
-    })
-  },
-  bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryIndex: e.detail.value
-    })
-  },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      accountIndex: e.detail.value
-    })
-  },
   bindAgreeChange: function (e) {
     this.setData({
       isAgree: !!e.detail.value.length
